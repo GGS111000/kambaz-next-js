e09fa0599e5cd7b457e953dbdf109c3528413b56
GGS111000 <cuixj2011@gmail.com>
2025-10-11 22:58:59 -0500

    a3 JavaScript7

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,31 +1,26 @@
 import { ReactNode } from "react";
-import { FaAlignJustify } from "react-icons/fa";
 import CourseNavigation from "./Navigation";
 import KambazNavigation from "../../Navigation";
 import { courses } from "../../Database";
-import Breadcrumb from "./Breadcrumb"; 
+import Breadcrumb from "./Breadcrumb";
 
-export default function CourseLayout({
-  children,
-  params,
-}: {
+
+export default async function CourseLayout(props: {
   children: ReactNode;
-  params: { cid: string };
+  params: Promise<{ cid: string }>;
 }) {
-  const { cid } = params;
-  const course = courses.find((course) => course._id === cid);
+  const { children, params } = props;
+  const { cid } = await params;
+  const course = courses.find((c) => c._id === cid);
 
   return (
     <div id="wd-courses" className="d-flex">
       <KambazNavigation />
-
       <div className="flex-fill" style={{ marginLeft: 120, padding: 20 }}>
         <Breadcrumb course={course} />
-
         <div className="d-flex">
-          <CourseNavigation params={{ cid }}/>
-
-
+  
+          <CourseNavigation params={{ cid }} />
           <div className="flex-fill" style={{ marginLeft: 150, padding: 20 }}>
             {children}
           </div>
