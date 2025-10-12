4a80ae74482290c49dbfc915dcc74b40554ea48e
GGS111000 <cuixj2011@gmail.com>
2025-10-11 22:47:26 -0500

    a3 JavaScript6

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -3,7 +3,7 @@ import { FaAlignJustify } from "react-icons/fa";
 import CourseNavigation from "./Navigation";
 import KambazNavigation from "../../Navigation";
 import { courses } from "../../Database";
-import Breadcrumb from "./Breadcrumb"; // ✅ 新增导入
+import Breadcrumb from "./Breadcrumb"; 
 
 export default function CourseLayout({
   children,
@@ -17,19 +17,15 @@ export default function CourseLayout({
 
   return (
     <div id="wd-courses" className="d-flex">
-      {/* ✅ 左侧 Kambaz 全局导航 */}
       <KambazNavigation />
 
-      {/* ✅ 右侧主区域 */}
       <div className="flex-fill" style={{ marginLeft: 120, padding: 20 }}>
-        {/* ✅ 面包屑（替代原 h2 + hr） */}
         <Breadcrumb course={course} />
 
         <div className="d-flex">
-          {/* ✅ 左侧课程导航 */}
-          <CourseNavigation />
+          <CourseNavigation params={{ cid }}/>
 
-          {/* ✅ 右侧内容区 */}
+
           <div className="flex-fill" style={{ marginLeft: 150, padding: 20 }}>
             {children}
           </div>
